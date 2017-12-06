27f31eaf77525d464544831bf7441478cd563179
macneko-ayu <macneko.ayu@gmail.com>
2017-12-06 15:59:03 +0900

    Add xcode project

---
+++ LaunchAppFromBundleIdentifier/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LaunchAppFromBundleIdentifier
+//
+//  Created by 横田孝次郎 on 2017/12/06.
+//  Copyright © 2017年 macneko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LaunchAppFromBundleIdentifier/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LaunchAppFromBundleIdentifier
+//
+//  Created by 横田孝次郎 on 2017/12/06.
+//  Copyright © 2017年 macneko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
