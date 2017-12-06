0674555a0089cf1ca4a2d177ae12affae130e3d3
macneko-ayu <macneko.ayu@gmail.com>
2017-12-06 17:15:03 +0900

    Add view model and unit test

---
+++ LaunchAppFromBundleIdentifier/ViewModel/AppListViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  AppListViewModel.swift
+//  LaunchAppFromBundleIdentifier
+//
+//  Created by 横田孝次郎 on 2017/12/06.
+//  Copyright © 2017年 macneko. All rights reserved.
+//
+
+import Cocoa
+
+struct AppListViewModel {
+    var items = [AppModel]()
+    
+    init(bundleIdentifier: String) {
+        
+    }
+}
---
+++ LaunchAppFromBundleIdentifierTests/AppListViewModelTests.swift
@@ -0,0 +1,28 @@
+//
+//  AppListViewModelTests.swift
+//  LaunchAppFromBundleIdentifierTests
+//
+//  Created by 横田孝次郎 on 2017/12/06.
+//  Copyright © 2017年 macneko. All rights reserved.
+//
+
+import XCTest
+@testable import LaunchAppFromBundleIdentifier
+
+class AppListViewModelTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testInitialize() {
+        let viewModel = AppListViewModel(bundleIdentifier: "com.apple.dt.xcode")
+        XCTAssertTrue(viewModel.items.count == 0)
+    }
+}
