8705267b1df8b5b3d367a63c5edc8072f7b596b3
yokota <macneko.ayu@gmail.com>
2017-12-07 14:46:12 +0900

    Add comment

--- LaunchAppFromBundleIdentifier/ViewModel/AppListViewModel.swift
+++ LaunchAppFromBundleIdentifier/ViewModel/AppListViewModel.swift
@@ -9,13 +9,17 @@
 import Cocoa
 
 struct AppListViewModel {
+    // icon画像のサイズをstaticで保持
     static let iconSize = NSSize(width: 120, height: 120)
     var items = [AppModel]()
     
     init?(bundleIdentifier: String) {
+        // BundleIdentifierにマッチするApplicationのファイルUrlを取得
+        // 同じBundleIdentifierのApplicationがインストールされている場合もあり得るので配列となる
         guard let applicationUrls = LSCopyApplicationURLsForBundleIdentifier(bundleIdentifier as CFString, nil)?.takeUnretainedValue() as? [URL] else {
             return nil
         }
+        // モデルを作成
         for applicationUrl in applicationUrls {
             guard let model = AppModel(fileUrl: applicationUrl, iconSize: AppListViewModel.iconSize) else {
                 break
